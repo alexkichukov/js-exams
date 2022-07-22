fdd4a17f42d4ace004837022364c85a09946f82d
Alex K <alex.kichukovv@gmail.com>
2022-07-22 18:03:21 +0300

    Fixed 2_2 to check for object value equality (regardless of keys)

--- Chapter_2/exam_2_2.ts
+++ Chapter_2/exam_2_2.ts
@@ -23,40 +23,46 @@ function findObj_Part2(input: any[] | Object, obj: Object) {
 
 // Part 3
 function findObj_Part3(input: any[] | Object, obj: Object) {
-  // Compare two object for non referential equality (deep)
-  const objectEquals = (obj1: Object, obj2: Object) => {
+  // Returns true if the two objects have the same values regardless of keys (deep)
+  const objectsHaveSameValues = (obj1: Object, obj2: Object) => {
     // Helper type checker
     const isObj = (a: any): boolean => typeof a === 'object' && a !== null;
 
     // If one of these are not objects then they can't be equal
     if (!isObj(obj1) || !isObj(obj2)) return false;
 
-    // If they dont have the same amount of keys they are already not equal
-    const keys = Object.keys(obj1);
-    if (keys.length !== Object.keys(obj2).length) return false;
+    // Get an array of all object values (deep)
+    const getValues = (obj: Object): unknown[] => {
+      const values = Object.values(obj);
 
-    for (let i = 0; i < keys.length; i++) {
-      const value1 = obj1[keys[i] as keyof typeof obj1];
-      const value2 = obj2[keys[i] as keyof typeof obj2];
-
-      // If values not of the same type they are not equal
-      if (typeof value1 !== typeof value2) return false;
-      
-      // Recursively check any values that are objects
-      if (isObj(value1) && isObj(value2)) {
-        if (!objectEquals(value1, value2)) return false;
-      } 
-      // And compare other values directly
-      else if (value1 !== value2) {
-        return false;
+      // Check all values for any objects, if there are any then remove them from 
+      // the array and add their values in place instead 
+      for (let i = 0; i < values.length; i++) {
+        if (isObj(values[i])) {
+          values.splice(i, 1, ...getValues(values[i]));
+        }
       }
+
+      return values;
+    }
+
+    const values1 = getValues(obj1);
+    const values2 = getValues(obj2);
+
+    // Not the same amount of values means they arent equal
+    if (values1.length !== values2.length) return false;
+
+    // If the two arrays dont have the same
+    for (let i = 0; i < values1.length; i++) {
+      if (values1.includes(values2[i])) values2.splice(i, 1);
+      else return false;
     }
 
     return true;
   }
 
   for (const item of Object.values(input)) {
-    if (objectEquals(obj, item)) return true;
+    if (objectsHaveSameValues(obj, item)) return true;
 
     // Recursively search all objects (or arrays)
     if (typeof item === 'object' && item !== null) {
@@ -67,47 +73,53 @@ function findObj_Part3(input: any[] | Object, obj: Object) {
   return false;
 }
 
-// Part 3
+// Part 4
 function findObj_Part4(input: any[] | Object, obj: Object) {
-  // Compare two object for non referential equality (deep)
-  const objectEquals = (obj1: Object, obj2: Object) => {
+  // Returns true if the two objects have the same values regardless of keys (deep)
+  const objectsHaveSameValues = (obj1: Object, obj2: Object) => {
     // Helper type checker
     const isObj = (a: any): boolean => typeof a === 'object' && a !== null;
 
     // If one of these are not objects then they can't be equal
     if (!isObj(obj1) || !isObj(obj2)) return false;
 
-    // If they dont have the same amount of keys they are already not equal
-    const keys = Object.keys(obj1);
-    if (keys.length !== Object.keys(obj2).length) return false;
+    // Get an array of all object values (deep)
+    const getValues = (obj: Object): unknown[] => {
+      const values = Object.values(obj);
 
-    for (let i = 0; i < keys.length; i++) {
-      const value1 = obj1[keys[i] as keyof typeof obj1];
-      const value2 = obj2[keys[i] as keyof typeof obj2];
-
-      // If values not of the same type they are not equal
-      if (typeof value1 !== typeof value2) return false;
-      
-      // Recursively check any values that are objects
-      if (isObj(value1) && isObj(value2)) {
-        if (!objectEquals(value1, value2)) return false;
-      } 
-      // And compare other values directly
-      else if (value1 !== value2) {
-        return false;
+      // Check all values for any objects, if there are any then remove them from 
+      // the array and add their values in place instead 
+      for (let i = 0; i < values.length; i++) {
+        if (isObj(values[i])) {
+          values.splice(i, 1, ...getValues(values[i]));
+        }
       }
+
+      return values;
+    }
+
+    const values1 = getValues(obj1);
+    const values2 = getValues(obj2);
+
+    // Not the same amount of values means they arent equal
+    if (values1.length !== values2.length) return false;
+
+    // If the two arrays dont have the same
+    for (let i = 0; i < values1.length; i++) {
+      if (values1.includes(values2[i])) values2.splice(i, 1);
+      else return false;
     }
 
     return true;
   }
 
   for (const item of Object.values(input)) {
-    // Check both reference and values
-    if (obj === item && objectEquals(obj, item)) return true;
+    // Check for either reference and values
+    if (obj === item || objectsHaveSameValues(obj, item)) return true;
 
     // Recursively search all objects (or arrays)
     if (typeof item === 'object' && item !== null) {
-      if (findObj_Part3(item, obj)) return true;
+      if (findObj_Part4(item, obj)) return true;
     }
   }
 
@@ -118,7 +130,7 @@ function findObj_Part4(input: any[] | Object, obj: Object) {
 // const person = { name: 'John Doe' };
 // const input1 = [6, "Test", "value", person, 1, undefined, null,  () => { console.log("Hello,  world!") }, { count: 5 }, { name: "John Doe" }];
 // const input2 = [6, "Test", "value", 1, undefined, null,  () => { console.log("Hello,  world!") }, { count: 5 }, { name: "John Doe", person }];
-// const input3 = [6, "Test", "value", 1, undefined, null,  () => { console.log("Hello,  world!") }, { count: 5 }, { name: "John Doe" }];
+// const input3 = [6, "Test", 1, undefined, null, () => { console.log("Hello,  world!") }, { count: 5 }, { randomStuff: { another: "John Doe" } }];
 // const input4 = [6, "Test", "value", 1, undefined, null,  () => { console.log("Hello,  world!") }, { count: 5 }, { name: "John Doe", person }];
 // console.log(findObj_Part1(input1, person));
 // console.log(findObj_Part2(input2, person));
